1bc9ed591e77a2943a115a6a320ef3e0beccfe49
Nikolay Dementiev <nikolas.dementiev@gmail.com>
2016-09-03 14:05:12 +0300

    start project

---
+++ ObjC-HelloWorld/ObjC-HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC-HelloWorld
+//
+//  Created by Nikolay Dementiev on 03.09.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
