be4f202622405294ba9a06140f33b3303eb4ceeb
Nikolay Dementiev <nikolas.dementiev@gmail.com>
2016-09-06 14:28:59 +0300

    1

--- ObjC-HelloWorld/ObjC-HelloWorld/ViewController.h
+++ ObjC-HelloWorld/ObjC-HelloWorld/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+//- (CGFloat)circleArea:(CGFloat)theRadius;
 
 @end
 
--- ObjC-HelloWorld/ObjC-HelloWorld/ViewController.m
+++ ObjC-HelloWorld/ObjC-HelloWorld/ViewController.m
@@ -14,14 +14,67 @@
 
 @implementation ViewController
 
+float circleArea(float theRadius);
+
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+		[super viewDidLoad];
+		// Do any additional setup after loading the view, typically from a nib.
+
+		//		UILabel *helloWorld = [[UILabel alloc] initWithFrame:CGRectMake(100.0f, 100.0f, 100.0f, 20.0f)];
+		//		helloWorld.backgroundColor = [UIColor clearColor];
+		//		helloWorld.text = @"Hello world";
+		//		[self.view addSubview:helloWorld];
+		//		//[helloWorld release];
+
+		//		float pictureWidth, pictureHeight, pictureSurfaceArea,
+		//		circleRadius, circleSurfaceArea;
+		//		pictureWidth = 8.0;
+		//		pictureHeight = 4.5;
+		//		circleRadius = 5.0;
+		//		pictureSurfaceArea = pictureWidth * pictureHeight;
+		//
+		//		circleSurfaceArea = circleArea(circleRadius);
+		//
+		//		NSLog(@"%f", circleSurfaceArea);
+
+		//		CGFloat pictureWidth, pictureHeight, pictureSurfaceArea,
+		//		circleRadius, circleSurfaceArea;
+		//		pictureWidth = 8.0;
+		//		pictureHeight = 4.5;
+		//		circleRadius = 5.0;
+		//		pictureSurfaceArea = pictureWidth * pictureHeight;
+		//
+		//		circleSurfaceArea = [self circleArea:circleRadius];
+		//		NSLog(@"%f", circleSurfaceArea);
+
+		NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+		NSString *result = [userDefaults objectForKey:@"name"];
+		if ([result length]) {
+    NSLog(@"name = %@", [userDefaults objectForKey:@"name"]);
+    NSLog(@"age = %li", (long)[userDefaults integerForKey:@"age"]);
+    NSLog(@"student = %i", [userDefaults boolForKey:@"student"]);
+		} else {
+    NSLog(@"Add values to UserDefaults");
+    [userDefaults setObject:@"Ira" forKey:@"name"];
+    [userDefaults setInteger:24 forKey:@"age"];
+    [userDefaults setBool:YES forKey:@"student"];
+    [userDefaults synchronize];
+		}
+
 }
 
+//float circleArea(float theRadius)
+//{
+//		return (3.14159 * theRadius * theRadius);
+//}
+
+//- (CGFloat)circleArea:(CGFloat)theRadius {
+//		return (3.14159 * theRadius * theRadius);
+//}
+
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+		[super didReceiveMemoryWarning];
+		// Dispose of any resources that can be recreated.
 }
 
 @end
